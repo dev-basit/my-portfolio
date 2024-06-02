004c67ec2d523a71554ba97cb015ad7b60d87f8b
Abdul Basit <dev.basit2020@gmail.com>
2024-06-02 10:33:41 +0500

    changed title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Snr Developer - 3+YOE</title>
+        <title>Snr Developer - 3+ YOE</title>
         <meta name="description" content="Software Developer - Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
